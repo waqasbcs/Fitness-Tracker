e38f3d0c9b4f48648f840171236684f6c83c0e3a
waqasbcs <waqasaliy2945@gmail.com>
2024-07-07 09:32:24 +0500

    model has been created

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    duration = models.DurationField()
+    notes = models.TextField(blank=True, null=True)
+
+class Exercise(models.Model):
+    workout = models.ForeignKey(Workout, related_name='exercises', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    repetitions = models.IntegerField()
+    sets = models.IntegerField()
+
+class Nutrition(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    calories = models.IntegerField()
+    protein = models.FloatField()
+    carbs = models.FloatField()
+    fat = models.FloatField()
+
+class Progress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    weight = models.FloatField()
+    body_fat_percentage = models.FloatField()
+
